3a17fe4c512f0794e1b5f482afbe41669803c530
zoranaism <lazareviczorana@gmail.com>
2020-04-07 20:49:51 +0200

    Added user in Products query and component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -13,7 +13,6 @@ export default function Navigation() {
   const token = useSelector(selectToken);
 
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
-  // const loginLogoutControls = <LoggedIn />;
 
   return (
     <Navbar bg="light" expand="lg">
@@ -24,33 +23,12 @@ export default function Navigation() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
           <NavbarItem path="/" linkText="Products" />
+          <NavbarItem path="/support" linkText="Support" />
         </Nav>
       </Navbar.Collapse>
       <Navbar.Collapse className="justify-content-end">
       {loginLogoutControls}
       </Navbar.Collapse>
     </Navbar>
-
-    // <Navbar bg="light" expand="lg">
-    //   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //   <Navbar.Collapse id="basic-navbar-nav">
-    //     <Nav className="mr-auto">
-    //       <Nav.Link href="#home">Home</Nav.Link>
-    //       <Nav.Link href="#link">Link</Nav.Link>
-          // <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          //   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          //   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          //   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          //   <NavDropdown.Divider />
-          //   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          // </NavDropdown>
-    //     </Nav>
-    //     <Form inline>
-    //       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-    //       <Button variant="outline-success">Search</Button>
-    //     </Form>
-    //   </Navbar.Collapse>
-    // </Navbar>
   );
 }
