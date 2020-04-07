42fc16cacf3d09fb293b3fee3c045f8193ff5cfe
Sandra <sandra.iriandini@gmail.com>
2020-04-07 19:29:59 +0200

    Add support link in the navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -20,14 +20,16 @@ export default function Navigation() {
       <Navbar.Brand as={NavLink} to="/">
         Telephone Webshop
       </Navbar.Brand>
+
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
           <NavbarItem path="/" linkText="Products" />
+          <NavbarItem path="/support" linkText="Support" />
         </Nav>
       </Navbar.Collapse>
       <Navbar.Collapse className="justify-content-end">
-      {loginLogoutControls}
+        {loginLogoutControls}
       </Navbar.Collapse>
     </Navbar>
 
@@ -38,13 +40,13 @@ export default function Navigation() {
     //     <Nav className="mr-auto">
     //       <Nav.Link href="#home">Home</Nav.Link>
     //       <Nav.Link href="#link">Link</Nav.Link>
-          // <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          //   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          //   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          //   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          //   <NavDropdown.Divider />
-          //   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          // </NavDropdown>
+    // <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+    //   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+    //   <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+    //   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+    //   <NavDropdown.Divider />
+    //   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+    // </NavDropdown>
     //     </Nav>
     //     <Form inline>
     //       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
