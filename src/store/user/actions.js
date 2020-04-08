99c837ed65ca3f8f13ab4fca2b37d2e771af204c
WanderSmits <wandersmits@gmail.com>
2020-04-08 17:01:26 +0200

    signup_in_redux

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -27,7 +27,7 @@ const tokenStillValid = (userWithoutToken) => ({
 
 export const logOut = () => ({ type: LOG_OUT });
 
-export const signUp = (name, email, password) => {
+export const signUp = (name, email, password, isOwner) => {
   return async (dispatch, getState) => {
     dispatch(appLoading());
     try {
@@ -35,6 +35,7 @@ export const signUp = (name, email, password) => {
         name,
         email,
         password,
+        isOwner,
       });
 
       dispatch(loginSuccess(response.data));
