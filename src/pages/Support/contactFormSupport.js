630c82c7cf70934805f2e281b490e6d95c69dfb5
Sandra <sandra.iriandini@gmail.com>
2020-04-08 13:30:51 +0200

    Modify Contact us form

--- src/pages/Support/contactFormSupport.js
+++ src/pages/Support/contactFormSupport.js
@@ -5,8 +5,9 @@ import { useDispatch } from "react-redux";
 
 export default function ContactFormSupport() {
   const dispatch = useDispatch();
-  const [name, setName] = useState("");
-  const [question, setQuestion] = useState("");
+  const [subject, setSubject] = useState("");
+  const [description, setDescription] = useState("");
+  const [link, setLink] = useState("");
 
   function submitForm(event) {
     event.preventDefault();
@@ -19,14 +20,9 @@ export default function ContactFormSupport() {
     <Container>
       <Form as={Col} md={{ span: 5, offset: 3 }} className="mt-5">
         <h3 className="mt-5 mb-5">Contact Form</h3>
-        <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="Name" placeholder="Your Name" />
-          value={question}
-          onChange={(event) => setQuestion(event.target.value)}
-        </Form.Group>
+
         <Form.Group controlId="exampleForm.ControlSelect1">
-          <Form.Label>What's this about ? </Form.Label>
+          <Form.Label>Reason for contacting us </Form.Label>
           <Form.Control as="select">
             <option>Delivery</option>
             <option>Opening time</option>
@@ -36,15 +32,39 @@ export default function ContactFormSupport() {
             <option>Other</option>
           </Form.Control>
         </Form.Group>
+
         <Form.Group>
-          <Form.Label>Question</Form.Label>
+          <Form.Label>Subject</Form.Label>
           <Form.Control
+            rows="5"
             type="text"
-            placeholder="Your Question"
-            value={question}
-            onChange={(event) => setQuestion(event.target.value)}
+            placeholder="Your Subject"
+            value={subject}
+            onChange={(event) => setSubject(event.target.value)}
           />
         </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Your Description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Link</Form.Label>
+          <Form.Control
+            rows="5"
+            type="text"
+            placeholder="Your Link"
+            value={link}
+            onChange={(event) => setLink(event.target.value)}
+          />
+        </Form.Group>
+
         <Button> Send</Button>
       </Form>
     </Container>
